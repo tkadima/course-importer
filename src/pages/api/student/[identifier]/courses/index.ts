d770b624f947d57e79b78bf2e40e3bb384986cef
tkadima <tabithakadima@gmail.com>
2024-09-24 16:50:16 -0400

    Get grades for a single student

--- src/pages/api/student/[identifier]/courses/index.ts
+++ src/pages/api/student/[identifier]/courses/index.ts
@@ -9,7 +9,7 @@ export default async function handler(
   try {
     const db = await getDb()
     const { identifier, type, ...otherParams } = req.query
-    // Get all classes for a student by id or email for a student (name is not unique)
+    // Get all course for a student by id or email for a student (name is not unique)
     // localhost:3000/api/student/{identifier}/classes?type={type}
     if (req.method === 'GET') {
       if (!identifier || !type) {
@@ -19,16 +19,19 @@ export default async function handler(
       }
 
       let query: string
+      // add more params for semester and year
 
       if (type === 'id') {
-        query = `SELECT class.* FROM enrollment JOIN class ON class.id = enrollment.class_id WHERE student_id = '${identifier}'`
+        query = `SELECT course.* FROM enrollment JOIN class ON class.id = enrollment.class_id JOIN course ON class.course_id = course.id WHERE student_id = '${identifier}'`
       } else if (type === 'email') {
-        query = `SELECT class.* FROM enrollment JOIN class ON class.id = enrollment.class_id JOIN student ON student.id = enrollment.student_id WHERE email = '${identifier}'`
+        query = `SELECT course.* FROM enrollment JOIN class ON class.id = enrollment.class_id JOIN course ON class.course_id = course.id JOIN student ON student.id = enrollment.student_id WHERE email = '${identifier}'`
       } else {
         return res.status(400).json({ error: 'Invalid type' })
       }
+      console.log('otherParams:', otherParams)
 
       let { params, newQuery } = getParams(query, otherParams, true)
+      console.log('newQuery:', newQuery)
 
       const data = await db.all(newQuery, params)
       res.status(200).json(data)
